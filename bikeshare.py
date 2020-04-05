fa028499610e3c9ce815d514e0264fc8b5f21bad
pss2138 <pss2138@gmail.com>
2020-04-05 11:49:38 +0900

    telling anyone can ask

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 """See?"""
+"""Tell me if you have any question"""
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
